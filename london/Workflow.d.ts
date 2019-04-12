feee5ca32695e951f1ade0aa68ac991f325b2254
Erik Myrold <emyrold@me.com>
2019-04-12 08:45:51 -0500

    saving workflow edits

--- london/Workflow.d.ts
+++ london/Workflow.d.ts
@@ -30,13 +30,109 @@ declare namespace global {
 
         /**
          * Adds a debug message to the log.
-         * @param message The message to add to the log.
+         * @param message The message to agit bdd to the log.
          * @param {Object} args Arguments to add to the message.
          * @returns The message added to the log.
          * @example
+         *
          * var loggedMessage = workflow.debug("All is well");
          */
         debug(message: string, args: object) {}
+
+        /**
+         * Adds an error message to the log.
+         * @param {String} message The message to add to the log.
+         * @param {Object} args Arguments to add to the message.
+         * @returns The logged message
+         * @example
+         *
+         * var loggedMessage = workflow.error("An error has occurred. ");
+         */
+        error(message: string, args: object) {}
+
+        /**
+         * Returns the specified variable's value.
+         * @param name The variable name
+         * @returns The variable's value
+         * @example
+         *
+         * var value = workflow.getVariable("task");
+         */
+        getVariable(name: string) {}
+
+        /**
+         * Adds an informational message to the log.
+         * @param message The message to add to the log.
+         * @param args Arguments to add to the message.
+         * @returns The message that is logged.
+         * @example
+         *
+         * var loggedMessage = workflow.info("All is well");
+         */
+        info(message: string, args: object) {}
+
+        /**
+         * Returns the workflow name.
+         * @returns The workflow name
+         * @example
+         *
+         * var name = workflow.name();
+         */
+        name() {}
+
+        /**
+         * Removes the specified variable from the workflow.
+         * @param name The variable name
+         * @returns Method does not return a value
+         * @example
+         *
+         * var value = workflow.removeVariable("task");
+         */
+        // orig > removeVariable(name) {}
+        removeVariable(name: sting): void;
+
+        /**
+         * Returns the workflow's scratchpad object.
+         * @returns The scratchpad object.
+         * @example
+         *
+         * var scratchpad = workflow.scratchpad();
+         */
+        scratchpad() {}
+
+        /**
+         * Sets the workflow's result.
+         * @param result The workflow's result
+         * @returns Method does not return a value
+         * @example
+         *
+         * workflow.setResult("Success");
+         */
+        setResult(result: string) {}
+
+        /**
+         * Sets the specified variable to the specified value.
+         * @param name The variable name
+         * @param value The value to be assigned to the variable.
+         * @returns Method does not return a value
+         * @example
+         *
+         * workflow.setVariable("task", "terrible");
+         */
+        setVariable(name: string, value: object) {}
+
+        /**
+         * Adds a warning message to the log.
+         * @param message The message to add to the log.
+         * @param args Arguments to add to the message.
+         * @returns The logged message
+         * @example
+         *
+         * var loggedMessage = workflow.warn("Check your permissions.");
+         */
+        warn(message: string, args: object) {}
+
+        /******   This is where I left off at...  ********/
 
         broadcastEvent(contextId: string, eventName: string): void;
         cancel(record: ScopedGlideRecord): void;
